47c6c807d8128e919cdd5b025e157dd6bc35ad11
Wahyudi120505 <wahyutjg123@gmail.com>
2024-06-30 23:09:19 +0700

    kuis6

---
+++ latihan_js.js
@@ -0,0 +1,7 @@
+
+console.log(`
+    Nama :  ${biodata.name_firs} ${biodata.name_last}
+    Umur :  ${biodata.age}
+    Gender :  ${biodata.gender}
+    Hobi :  ${biodata.hobbies}
+`);
---
+++ objek/objek.js
@@ -0,0 +1,19 @@
+let car = {
+    // properti (variable)
+    type: "Fiat",
+    model: "500",
+    color: "white",
+    // method (function)
+    start: function () {
+        console.log("Ini adalah methode Start ")
+    },
+    drive: function () {
+        console.log("Ini adalah methode Drive")
+    },
+    brake: function () {
+        console.log("Ini adalah methode Brake")
+    },
+    stop: function () {
+        console.log("Ini adalah methode Stop")
+    }
+}
